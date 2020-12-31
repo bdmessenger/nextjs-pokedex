2993d40b0cb8e01abeec671989871b0f168424a6
Brant <bran.bd@gmail.com>
2020-12-31 12:30:48 -0600

    Fixed case sensitive on next/link import

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import Link from 'next/Link';
+import Link from 'next/link';
 import Layout from '../components/Layout'
 import { PokemonContext } from '../components/PokemonContextProvider'
 
